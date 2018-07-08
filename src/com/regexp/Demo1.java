403d9e273fa680f9239c8bf6cbc1d5fd5594f8c4
linzlb <798648334@qq.com>
2018-07-08 17:41:40 +0800

    v1.3

--- src/com/regexp/Demo1.java
+++ src/com/regexp/Demo1.java
@@ -2,21 +2,20 @@ package com.regexp;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//判断是否匹配
+
 public class Demo1 {
 	public static void main(String[] args) {
 		Pattern pattern = null;
 		try
 		{
-			// 输入正则表达式
 			//pattern = Pattern.compile( "\\d\\d\\d-\\d\\d\\d\\d\\d\\d\\d\\d" );
 			pattern = Pattern.compile( "\\d{3,4}-\\d{7,8}" );
-			// 绑定要判断的字符串
+
 			Matcher matcher = pattern.matcher( "020-12345678" );
-			boolean result = matcher.matches();// 得到匹配结果
+			boolean result = matcher.matches();// ????????
 			
 			Matcher matcher2 = pattern.matcher( "0220-1234578" );
-			boolean result2 = matcher2.matches();// 得到匹配结果
+			boolean result2 = matcher2.matches();// ????????
 					
 			System.out.println( result+"-"+result2 );
 		} catch (Exception e) {
