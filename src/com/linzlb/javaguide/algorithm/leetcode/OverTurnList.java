133a1f5df2bfd32b1d7b05d5c40b8a30d321e796
linzlb <798648334@qq.com>
2021-02-05 14:15:46 +0800

    msg:labuladong code 纯递归反转链表

--- src/com/linzlb/javaguide/algorithm/leetcode/OverTurnList.java
+++ src/com/linzlb/javaguide/algorithm/leetcode/OverTurnList.java
@@ -8,17 +8,27 @@ package com.linzlb.javaguide.algorithm.leetcode;
  */
 public class OverTurnList {
 
+    //正常反转
     public ListNode reverseList(ListNode head){
         ListNode next = null;
         ListNode pre = null;
         while (head!=null){
-
             next = head.next;
             head.next = pre;
             pre = head;
             head = next;
         }
         return pre;
+    }
+    //纯递归反转链表,要画图看，不然不好理解
+    public ListNode reverseList2(ListNode head){
+        if (head==null || head.next==null){
+            return head;
+        }
+        ListNode last = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
     }
 
     public static void main(String[] args) {
