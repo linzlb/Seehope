80aeea19610b059ae93c8c17a8668765879a2eb8
jiangwan <jiangwan.lin@tuya.com>
2022-03-04 21:39:16 +0800

    Seehope#linzlb msg: 用状态压缩优化dp数组处理递归问题

--- src/com/linzlb/labuladong/coreroutine/Fibonacci.java
+++ src/com/linzlb/labuladong/coreroutine/Fibonacci.java
@@ -20,6 +20,7 @@ public class Fibonacci {
 		//System.out.println("f(100)="+ instance.fibonacci(100));//半天都出不来
 		System.out.println("f(30)="+ instance.fibonacci2(30));
 		System.out.println("f(30)="+ instance.fibonacci3(30));
+		System.out.println("f(30)="+ instance.fibonacci4(30));
 	}
 
 	//直接递归，速度很慢，暴力递归 时间复杂度是2的n次方，指数级别
@@ -29,7 +30,7 @@ public class Fibonacci {
 		return fibonacci(n-1)+fibonacci(n-2);
 	}
 
-	//1.备忘录优化 自顶向下
+	//1.备忘录优化 自顶向下 时间复杂度：O(n) 空间复杂度：O(n)
 	Map<Integer, Integer> memo = new HashMap();
 	private int fibonacci2(int n){
 		return helper(memo, n);
@@ -45,7 +46,7 @@ public class Fibonacci {
 		return result;
 	}
 
-	//2.dp数组优化 自底向上
+	//2.dp数组优化 自底向上 时间复杂度：O(n) 空间复杂度：O(n)
 	private int fibonacci3(int n){
 		if (n<2){
 			return 1;
@@ -54,8 +55,28 @@ public class Fibonacci {
 		dp[0] = 1;
 		dp[1] = 1;
 		for (int i = 2; i <= n; i++)
-			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] = dp[i - 1] + dp[i - 2];//相当于动态规划的状态转移方程
 		return dp[n];
+	}
+
+	//3.自底向上的时候不需要存那么多次，因为每次只用到前2个的值 状态压缩！！！
+	// 时间复杂度：O(n) 空间复杂度：O(1)
+	private int fibonacci4(int n){
+		if (n<2){
+			return 1;
+		}
+		int dp[] = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		//用2个空间存前面2个状态
+		int prev = 1;
+		int curr = 1;
+		for (int i = 2; i <= n; i++){
+			int sum = prev + curr;
+			prev = curr;
+			curr = sum;
+		}
+		return curr;
 	}
 
 
